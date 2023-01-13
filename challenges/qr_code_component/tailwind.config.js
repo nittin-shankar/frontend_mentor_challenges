7f545eddd0a1099d7ce76ad9deedc7a111570d0b
Narandra Sridar <nittin.shankar@gmail.com>
2023-01-13 17:12:28 +0530

    Qr code component (#3)
    * Prepare QR code component challenge
    
    * Fix QR code component test
    
    Avoid potential issues with multiple links on the page.
    
    * Improve QR code component challenge
    
    Fix Tailwind CSS config.
    Improve test to save screenshot for two viewport sizes.
    
    * Fix credits position issue in QR code component
    
    * Create QR code component
    
    * Remove padding from body and add margin for component
    
    * Add google fonts and reduce the size of component
    
    * Run prettier plugin
    
    * Add credits to Optimum BH internship and have a link to source code
    
    * Put my username instead of `optimumBA` in the link to source code
    
    * Put my username inside the page qr code component instead of homepage
    
    * Add the optimum colors in configuration
    
    * Move all images used by the component to `challenges/qr_code_component/images`
    
    Co-authored-by: Almir Sarajčić <almir.sarajcic@icloud.com>

---
+++ challenges/qr_code_component/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./challenges/qr_code_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'grayish-blue': '#7B879D',
+        'light-gray': '#D6E2F0',
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+      },
+      fontFamily: {
+        outfit: ['"Outfit"'],
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
