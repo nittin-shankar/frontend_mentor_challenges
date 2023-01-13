145b4a70763bfaec9aa1e928066b976c0459fbaf
Narandra Sridar <nittin.shankar@gmail.com>
2023-01-13 17:10:31 +0530

    Interactive pricing component (#2)
    * Prepare Interactive pricing component challenge
    
    * Fix test
    
    * Create interactive prcing card
    
    * Add background SVGs to the page
    
    * Style the range slider as per the requirements
    
    * Update the interactive component with the actual values specified
    
    * Update the pricing for yearly and monthly billing
    
    * Make the design of the pricing component mobile friendly
    
    * Run prettier plugin
    
    Co-authored-by: Almir Sarajčić <almir.sarajcic@icloud.com>

---
+++ challenges/interactive_pricing_component/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./challenges/interactive_pricing_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        'dark-desaturated-blue': 'hsl(227, 35%, 25%)',
+        'pale-blue': 'hsl(226, 100%, 87%)',
+        'grayish-blue': 'hsl(225, 20%, 60%)',
+        'light-grayish-blue': 'hsl(224, 65%, 95%)',
+        'light-grayish-red': 'hsl(14, 92%, 95%)',
+        'light-red': 'hsl(15, 100%, 70%)',
+        'soft-cyan': 'hsl(174, 77%, 80%)',
+        'very-pale-blue': 'hsl(230, 100%, 99%)',
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
