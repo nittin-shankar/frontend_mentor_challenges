88eaf907435b3c7c2f9aff9d5bab245edb79e658
Narandra Sridar <nittin.shankar@gmail.com>
2023-01-13 17:16:55 +0530

    Order summary component (#7)
    * Prepare Order summary component challenge
    
    * Fix Order summary component test
    
    Avoid potential issues with multiple links on the page.
    
    * Improve Order summary component challenge
    
    Fix credits position issue.
    Fix Tailwind CSS config.
    Improve test to save screenshot for two viewport sizes.
    
    * Create layout for order summary component
    
    * Use specified colors for all elements
    
    * Add shadow to primary button
    
    * Use specified color for _plan_ card in component
    
    * Use specified google fonts for all text in component
    
    * Add background pattern on the page
    
    * Design for active states of buttons and links
    
    * Adjust padding for mobile viewy
    
    * Run prettier plugin
    
    * Change the github username to `nittin-shankar`
    
    * Move all images used by the component to `challenges/order_summary_component/images`
    
    Co-authored-by: Almir Sarajčić <almir.sarajcic@icloud.com>

---
+++ challenges/order_summary_component/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./challenges/order_summary_component/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'optimum-blue': '#009efc',
+        'optimum-darkblue': '#0389e1',
+        'pale-blue': 'hsl(225, 100%, 94%)',
+        'bright-blue': 'hsl(245, 75%, 52%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+        'desaturated-blue': 'hsl(224, 23%, 55%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+      },
+      fontFamily: {
+        'red-hat-display': ['Red Hat Display'],
+      },
+    },
+  },
+  plugins: [],
+  variants: {},
+}
